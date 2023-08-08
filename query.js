4ccfe1321958a29b07c7ab7d0858e092c5dad533
manan37 <mananbhasin5657@gmail.com>
2023-08-08 12:43:33 +0530

    first commit

---
+++ query.js
@@ -0,0 +1,6 @@
+var search = document.querySelector(".dropdown");
+var searchInput = document.querySelector(".input");
+
+// searchInput.addEventListener("click", () => {
+//   search.style.display = "block";
+// });
